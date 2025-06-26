85b42a21e11a575041b4559359a763b0e340886e
Scott Taylor <Scott2013taylor@gmail.com>
2025-06-26 13:55:40 +0100

    added 2011-13 data

--- main.py
+++ main.py
@@ -14,28 +14,20 @@ OUTPUT_FILE = "combined_output.csv"
 processed_cache = cache.load_cache()
 combined_df = pd.DataFrame()
 
-# === SHEET CONFIG ===
-# sheet_configs = [
-#     {"level": "Standard Grade", "folder": r"data\Standard Grade\SG", "sheets": ["SG5a", "SG5b"] },
-#     {"level": "Standard Grade", "folder": r"data\Standard Grade\IM2", "sheets": ["IB4a", "IB4b"]},
-#     {"level": "Higher", "folder": r"data\Higher\2000-2002", "sheets": ["NH4a", "NH4b"]},
-#     {"level": "Higher", "folder": r"data\Higher\2003-2010", "sheets": ["NH5a", "NH5b"]},
-#     {"level": "Advanced Higher", "folder": r"data\Advanced Higher\2000-2000", "sheets": ["CS3a", "CS3b"]},
-#     {"level": "Advanced Higher", "folder": r"data\Advanced Higher\2001-2002", "sheets": ["AH4a", "AH4b"]},
-#     {"level": "Advanced Higher", "folder": r"data\Advanced Higher\2003-2010", "sheets": ["AH5a", "AH5b"]},
-# ]
-
 sheet_configs = [
     SheetConfig("Standard Grade", r"data\Standard Grade\SG", ["SG5a", "SG5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Standard Grade", r"data\Standard Grade\IM2", ["IB4a", "IB4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Standard Grade", r"data\Standard Grade\2010-2010", ["SG5a", "SG5b"], gender_cell="A4", year_cell="A1"),
+    SheetConfig("Standard Grade", r"data\Standard Grade\2011-2012", ["SG5a", "SG5b"], gender_cell="A6", year_cell="A3"),
     SheetConfig("Higher", r"data\Higher\2000-2002", ["NH4a", "NH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2003-2009", ["NH5a", "NH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2010-2010", ["NH5a", "NH5b"], gender_cell="A4", year_cell="A1"),
+    SheetConfig("Higher", r"data\Higher\2011-2012", ["NH5a", "NH5b"], gender_cell="A6", year_cell="A3"),
     SheetConfig("Advanced Higher", r"data\Advanced Higher\2000-2000", ["CS3a", "CS3b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced Higher", r"data\Advanced Higher\2001-2002", ["AH4a", "AH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced Higher", r"data\Advanced Higher\2003-2009", ["AH5a", "AH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced Higher", r"data\Advanced Higher\2010-2010", ["AH5a", "AH5b"], gender_cell="A4", year_cell="A1"),
+    SheetConfig("Advanced Higher", r"data\Advanced Higher\2011-2012", ["AH5a", "AH5b"], gender_cell="A6", year_cell="A3"),
 ]
 
 
