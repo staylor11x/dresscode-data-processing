5746bb9877043eda028b3369d5b9d0c7570dde2b
Scott Taylor <Scott2013taylor@gmail.com>
2025-07-02 16:57:34 +0100

    CSYS Added & STDG/INT2 files rearranged.

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ exclude_keywords = [
 ]
 
 sheet_configs = [
-    SheetConfig("Intermediate_2", r"data\Standard Grade\IM2", ["IB4a", "IB4b"], gender_cell="A3", year_cell="A1"),
-    SheetConfig("Standard_Grade", r"data\Standard Grade\SG", ["SG5a", "SG5b"], gender_cell="A3", year_cell="A1"),
-    SheetConfig("Standard_Grade", r"data\Standard Grade\2010-2010", ["SG5a", "SG5b"], gender_cell="A4", year_cell="A1"),
-    SheetConfig("Standard_Grade", r"data\Standard Grade\2011-2012", ["SG5a", "SG5b"], gender_cell="A6", year_cell="A3"),
+    # SheetConfig("Intermediate_2", r"data\Standard Grade\IM2", ["IB4a", "IB4b"], gender_cell="A3", year_cell="A1"),
+    # SheetConfig("Standard_Grade", r"data\Standard Grade\SG", ["SG5a", "SG5b"], gender_cell="A3", year_cell="A1"),
+    # SheetConfig("Standard_Grade", r"data\Standard Grade\2010-2010", ["SG5a", "SG5b"], gender_cell="A4", year_cell="A1"),
+    # SheetConfig("Standard_Grade", r"data\Standard Grade\2011-2012", ["SG5a", "SG5b"], gender_cell="A6", year_cell="A3"),
     SheetConfig("Higher", r"data\Higher\2000-2002", ["NH4a", "NH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2003-2009", ["NH5a", "NH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2010-2010", ["NH5a", "NH5b"], gender_cell="A4", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2011-2012", ["NH5a", "NH5b"], gender_cell="A6", year_cell="A3"),
-    SheetConfig("CSYS", r"data\Advanced Higher\2000-2000", ["CS3a", "CS3b"], gender_cell="A3", year_cell="A1"),
+    SheetConfig("CSYS", r"data\Advanced Higher\2000-2000", ["CS3a", "CS3b"], gender_cell="A4", year_cell="A1"),
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2001-2002", ["AH4a", "AH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2003-2009", ["AH5a", "AH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2010-2010", ["AH5a", "AH5b"], gender_cell="A4", year_cell="A1"),
