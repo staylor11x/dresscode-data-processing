8e2fc6e635222158390f1a019793fc59a024d9ac
Scott Taylor <Scott2013taylor@gmail.com>
2025-07-09 13:07:56 +0100

    INT1 Data added

--- main.py
+++ main.py
@@ -30,24 +30,29 @@ sheet_configs = [
     SheetConfig("Standard_Grade", r"data\Standard Grade\2000-2002", ["SG4a", "SG4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Standard_Grade", r"data\Standard Grade\2003-2009", ["SG5a", "SG5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Standard_Grade", r"data\Standard Grade\2010-2010", ["SG5a", "SG5b"], gender_cell="A4", year_cell="A1"),
-    SheetConfig("Standard_Grade", r"data\Standard Grade\2011-2012", ["SG5a", "SG5b"], gender_cell="A6", year_cell="A3"),
+    SheetConfig("Standard_Grade", r"data\Standard Grade\2011-2013", ["SG5a", "SG5b"], gender_cell="A6", year_cell="A3"),
     
     SheetConfig("Intermediate_2", r"data\Intermediate 2\2000-2002", ["IB4a", "IB4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Intermediate_2", r"data\Intermediate 2\2003-2009", ["IB5a","IB5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Intermediate_2", r"data\Intermediate 2\2010-2010", ["IB5a","IB5b"], gender_cell="A4", year_cell="A1"),
     SheetConfig("Intermediate_2", r"data\Intermediate 2\2011-2013", ["IB5a","IB5b"], gender_cell="A6", year_cell="A3"),
     
+    SheetConfig("Intermediate_1", r"data\Intermediate 1\2000-2002", ["IA4a", "IA4b"], gender_cell="A3", year_cell="A1"),
+    SheetConfig("Intermediate_1", r"data\Intermediate 1\2003-2009", ["IA5a", "IA5b"], gender_cell="A3", year_cell="A1"),
+    SheetConfig("Intermediate_1", r"data\Intermediate 1\2010-2010", ["IA5a", "IA5b"], gender_cell="A4", year_cell="A1"),
+    SheetConfig("Intermediate_1", r"data\Intermediate 1\2011-2013", ["IA5a", "IA5b"], gender_cell="A6", year_cell="A3"),
+    
     SheetConfig("Higher", r"data\Higher\2000-2002", ["NH4a", "NH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2003-2009", ["NH5a", "NH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Higher", r"data\Higher\2010-2010", ["NH5a", "NH5b"], gender_cell="A4", year_cell="A1"),
-    SheetConfig("Higher", r"data\Higher\2011-2012", ["NH5a", "NH5b"], gender_cell="A6", year_cell="A3"),
+    SheetConfig("Higher", r"data\Higher\2011-2013", ["NH5a", "NH5b"], gender_cell="A6", year_cell="A3"),
     
     SheetConfig("CSYS", r"data\Advanced Higher\2000-2000", ["CS3a", "CS3b"], gender_cell="A4", year_cell="A1"),
     
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2001-2002", ["AH4a", "AH4b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2003-2009", ["AH5a", "AH5b"], gender_cell="A3", year_cell="A1"),
     SheetConfig("Advanced_Higher", r"data\Advanced Higher\2010-2010", ["AH5a", "AH5b"], gender_cell="A4", year_cell="A1"),
-    SheetConfig("Advanced_Higher", r"data\Advanced Higher\2011-2012", ["AH5a", "AH5b"], gender_cell="A6", year_cell="A3"),
+    SheetConfig("Advanced_Higher", r"data\Advanced Higher\2011-2013", ["AH5a", "AH5b"], gender_cell="A6", year_cell="A3"),
 ]
 
 # === MAIN EXECUTION ===
